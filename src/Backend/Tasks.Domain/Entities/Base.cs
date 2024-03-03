3961ed10bb5d2975c6693e00330479fd67e071fb
vts1978 <vinicioussouza@gmail.com>
2024-03-03 16:15:42 -0300

    Fix: Removing Userid from user model, since now there is a baseentity

---
+++ src/Backend/Tasks.Domain/Entities/Base.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Domain.Entities;
+public class Base
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
--- src/Backend/Tasks.Domain/Entities/User.cs
+++ src/Backend/Tasks.Domain/Entities/User.cs
@@ -1,7 +1,6 @@
 ﻿namespace Tasks.Domain.Entities;
-public class User
+public class User : Base
 {
-    public int UserId { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }

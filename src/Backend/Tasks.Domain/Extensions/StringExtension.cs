27a64819a19c3f21f974f26d64a12ab65532bfb9
vts1978 <vinicioussouza@gmail.com>
2024-03-05 14:37:27 -0300

    Add: stringexrensions method, and repository interface to tasks

---
+++ src/Backend/Tasks.Domain/Extensions/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Tasks.Domain.Extensions;
+public static class StringExtension
+{
+    public static bool CompareWithoutConsideringSpecialCharactersAndUpperCase(this string origen, string searchFor)
+    {
+        var index = CultureInfo.CurrentCulture.CompareInfo.IndexOf(origen, searchFor, CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace);
+
+        return index >= 0;
+    }
+
+    public static string RemoveAccent(this string text)
+    {
+        return new string(text.Normalize(System.Text.NormalizationForm.FormD).Where(x => char.GetUnicodeCategory(x) != UnicodeCategory.NonSpacingMark).ToArray());
+    }
+}
---
+++ src/Backend/Tasks.Domain/Repositories/Tasks/ITaskUpdateOnlyRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Tasks.Domain.Repositories.Tasks;
+public interface ITaskUpdateOnlyRepository
+{
+    void Update(Entities.Task task);
+    Task<Entities.Task> GetById(long taskId);
+}

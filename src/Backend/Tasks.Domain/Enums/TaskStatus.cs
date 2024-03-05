8105f1711baa2d7206d9e45e6c93dec3fa76a0da
vts1978 <vinicioussouza@gmail.com>
2024-03-05 14:36:36 -0300

    Fix: enums values

--- src/Backend/Tasks.Domain/Enums/TaskStatus.cs
+++ src/Backend/Tasks.Domain/Enums/TaskStatus.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tasks.Domain.Enums;
+﻿namespace Tasks.Domain.Enums;
 public enum TaskStatus
 {
-    NotStarted,
-    InProgress,
-    Completed
+    NotStarted = 0,
+    InProgress = 1,
+    Completed = 2
 }
